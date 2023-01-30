2017789409ee6151f6276013ffacbd2397f6dae7
Sergei Aidinov <incoming34@yandex.ru>
2023-01-30 21:23:48 +0300

    Прикрутил OpenApi и сделал заготовку для контроллера

---
+++ src/main/java/com/example/tt_nsk/config/OpenApiConfig.java
@@ -0,0 +1,47 @@
+package com.example.tt_nsk.config;
+
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.context.annotation.Bean;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+
+@SpringBootConfiguration
+public class OpenApiConfig {
+
+    @Bean
+    OpenAPI customOpenApi() {
+        OpenAPI openAPI = new OpenAPI();
+        openAPI.setInfo(info());
+        return openAPI;
+    }
+
+    @Bean
+    Info info() {
+        return new Info().title("Администрирование «коммерческих соревнований» по настольному теннису")
+                .description("Среди участников турниров большая разница в уровне игры. " +
+                        "Большинство игр являются «формальными». " +
+                        "Либо быстрая и легкая победа, либо такое же быстрое поражение. " +
+                        "Решить данную проблему может система «гандикапа» (форы) - начальное преимущество, " +
+                        "намеренно предоставленное одной из сторон в соревновании, чтобы уравнять шансы " +
+                        "на победу при разных силах и возможностях соревнующихся.")
+                .version("1.0")
+                .license(licence())
+                .contact(contact());
+    }
+
+    @Bean
+    License licence() {
+        return new License()
+                .name("Unlicense")
+                .url("https://unlicense.org");
+    }
+
+    @Bean
+    Contact contact() {
+        return new Contact().email("incoming34@yandex.ru").name("Евгений Малюгин");
+    }
+
+}
---
+++ src/main/java/com/example/tt_nsk/controller/EnrollTournament.java
@@ -0,0 +1,19 @@
+package com.example.tt_nsk.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/enroll")
+public class EnrollTournament {
+
+    @GetMapping(value = "/upcomingTournaments")
+    public ResponseEntity getUpcomingTournaments(){
+        return null;
+    }
+
+
+
+}
