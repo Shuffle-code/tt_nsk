f4f7c698e16d6c508f86f608412fbbfdf0844342
Sergei Aidinov <incoming34@yandex.ru>
2023-02-19 21:30:48 +0300

    Добавил часовой пояс

--- src/main/java/com/example/tt_nsk/TtNskApplication.java
+++ src/main/java/com/example/tt_nsk/TtNskApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.awt.*;
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class TtNskApplication {
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 		SpringApplication.run(TtNskApplication.class, args);
 		System.setProperty("java.awt.headless", "false");
 
