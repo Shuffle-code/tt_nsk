5f882084d47348f76e53be2517f6f0dc325dd60e
Sergei Aidinov <incoming34@yandex.ru>
2023-02-20 17:52:06 +0300

    Восттановил утраченные скрипты liquibase

---
+++ src/main/resources/db/changelog/modifyTable/modify-table-tournament.sql
@@ -0,0 +1,6 @@
+--liquibase formatted sql
+--changeset liquibase:31eaef3b-c09c-40e3-a94a-685efeef982a
+
+ALTER TABLE tournament ADD COLUMN current_tournament TEXT;
+
+--rollback ALTER TABLE tournament DROP COLUMN current_tournament;
